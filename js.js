5b3a1708980020ae6b829c86f91dacb1982723a3
AndreyGrek <an.gee@icloud.com>
2020-02-15 17:24:00 +0300

    upd

--- js.js
+++ js.js
@@ -1,25 +1,69 @@
+function $(el){
+  return document.querySelector(el)
+}
+
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 canvas.width = document.body.clientWidth;
 canvas.height = window.innerHeight;
 
-const aX = 200;
-const aY = 50;
-const bX = 50;
-const bY = 350;
-const cX = 350;
-const cY = 350;
+//work place________________________________
 
-let initialX = aX;
-let initialY = aY;
+
+function createCircleManager(){
+  const circle = {a: {x: 300, y: 100}}
+
+  return {
+    printX: function(){
+      return circle.x
+    },
+    printY: function(){
+      return circle.y
+    },
+    print: function(){
+      return circle
+    },
+    addX: function(x){
+      circle.x = x
+    },
+    addY: function(y){
+      circle.y = y
+    }
+  }
+}
+
+const manager = createCircleManager()
+
+$("#x").addEventListener("input", function(){
+  manager.addX(parseInt(this.value));
+  console.log(manager.print());
+})
+
+$("#y").addEventListener("input", function(){
+  manager.addY(parseInt(this.value))
+})
+
+
+
+//work place________________________________
+
+let mainDots = {
+  a: {x: 300, y: 100}, 
+  b: {x: 100, y: 500}, 
+  c: {x: 500, y: 500}, 
+  d: {x: 300, y: 350}, 
+}
+
+let initialX = mainDots.a.x;
+let initialY = mainDots.a.y;
 
 ctx.fillRect(initialX, initialY, 1, 1);
 
 let countDots = 0;
 
 function textCount(){
-  document.querySelector("#count").innerHTML = `Точек: ${countDots}`;
+  $("#count").innerHTML = `Точек: ${countDots}`;
 }
 
 function circle(x, y){
@@ -27,80 +71,61 @@ function circle(x, y){
   ctx.arc(x, y, 3, 0, 2 * Math.PI, true);
   ctx.fill();
 }
-circle(aX, aY);
-circle(bX, bY);
-circle(cX, cY);
-
-//work place
-
-
-function createCircleManager(){
-  const circle = []
-
-  return {
-    printX: function(){
-      return circle
-    },
-    printY: function(){
-      return circle
-    },
-    add: function(x, y){
-      circle.push({x: x, y: y})
-    }
-  }
-}
-
-const manager = createCircleManager()
-
-document.querySelector(".submit").addEventListener("click", function(e){
-  manager.add(document.querySelector(".form__div form input[name=x]").value, document.querySelector(".form__div form input[name=y]").value)
-
-  e.preventDefault()
-
-  return circle(document.querySelector(".form__div form input[name=x]").value, document.querySelector(".form__div form input[name=y]").value);
-})
-
-
-
-//work place
-
-
+circle(mainDots.a.x, mainDots.a.y);
+circle(mainDots.b.x, mainDots.b.y);
+circle(mainDots.c.x, mainDots.c.y);
+circle(mainDots.d.x, mainDots.d.y);
 
 function dot(x, y){
   ctx.fillRect(x, y, 1, 1);
 }
 
-function move(){
-  return Math.floor(Math.random() * 4)
-}
-
 function game(){
-  let shot = move();
+  let shot = Math.floor(Math.random() * 4);
 
-  if(shot === 0){
-    initialX = Math.floor((initialX + aX) / 2);
-    initialY = Math.floor((initialY + aY) / 2);
-    dot(initialX, initialY);
-    countDots += 1;
-    textCount();
-  } if(shot === 1){
-    initialX = Math.floor((initialX + bX) / 2);
-    initialY = Math.floor((initialY + bY) / 2);
-    dot(initialX, initialY);
-    countDots += 1;
-    textCount();
-  } if(shot === 2){
-    initialX = Math.floor((initialX + cX) / 2);
-    initialY = Math.floor((initialY + cY) / 2);
+  function Other(){
     dot(initialX, initialY);
     countDots += 1;
     textCount();
   }
+
+  if(shot == 0){
+    initialX = Math.floor((initialX + mainDots.a.x) / 2);
+    initialY = Math.floor((initialY + mainDots.a.y) / 2);
+    Other()
+  } else if(shot == 1){
+    initialX = Math.floor((initialX + mainDots.b.x) / 2);
+    initialY = Math.floor((initialY + mainDots.b.y) / 2);
+    Other()
+  } else if(shot == 2){
+    initialX = Math.floor((initialX + mainDots.c.x) / 2);
+    initialY = Math.floor((initialY + mainDots.c.y) / 2);
+    Other()
+  } else if(shot == 3){
+    initialX = Math.floor((initialX + mainDots.d.x) / 2);
+    initialY = Math.floor((initialY + mainDots.d.y) / 2);
+    Other()
+  } 
 }
 
-function wrap(){
-  game();
-}
+let startToggle;
 
-setInterval(wrap, 1);
+
+$("#start").addEventListener("click", function() {
+  startToggle = setInterval(() => game(), 1);
+  // for(let i = 0; i < 1000; i++){
+  //   startToggle = setInterval(() => game(), 1);
+  // }
+  this.classList.add("off");
+  $("#stop").classList.remove("off");
+
+
+});
+$("#stop").addEventListener("click", function() {
+  clearInterval(startToggle, 1);
+  this.classList.add("off");
+  $("#start").classList.remove("off");
+});
+
+  
 
